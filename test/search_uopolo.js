170f10148e6915b4f320ad3cffa856c78e99f8ab
TestUser03 <74558702+TestUser03@users.noreply.github.com>
2021-08-25 03:59:59 +0900

    Add files via upload

--- test/search_uopolo.js
+++ test/search_uopolo.js
@@ -1,23 +1,39 @@
-const fs = require('fs');
+const { Builder, By, Capabilities, Key, until } = require('selenium-webdriver')
+const capabilities = Capabilities.chrome()
+// Chrome起動時のオプションを指定
+capabilities.set('chromeOptions', {
+  args: [
+    '--headless',
+    '--disable-gpu',
+    '--window-size=1024,768'
+  ],
+  w3c: false
+})
 
-const { sleep }= require('@theintern/leadfoot/lib/util');
+const search = async (query) => {
+  // オプション指定ありでChromeを起動
+  const driver = await new Builder()
+    .withCapabilities(capabilities)
+    .build()
+  try {
+    // Qiitaのトップページへアクセス
+    await driver.get('https://qiita.com/')
+    // 検索欄にキーワードを入力してEnter
+    await driver
+      .wait(until.elementLocated(By.name('q')), 5000)
+      .sendKeys(query, Key.RETURN)
+    // 検索結果数のバッジの値を取得
+    const result = await driver
+      .wait(until.elementLocated(By.className('badge')), 5000)
+      .getText()
+    // キーワードと結果の値を出力
+    console.log(`${query}: ${result}`)
+  } catch(e) {
+    console.log(e)
+  } finally {
+    // Chromeを終了
+    driver && await driver.quit()
+  }
+}
 
-const { describe, it } = intern.getPlugin('interface.bdd');
-const { expect } = intern.getPlugin('chai');
-
-describe('app', () => {
-    it('ヤフーにアクセスして「test」で検索', async test => {
-        const { remote } = test;
-        await remote
-            .get('http://www.yahoo.co.jp/')
-
-        await remote.setFindTimeout(5 * 1000);
-        await remote.findById('srchtxt').type('test');
-        await remote.findById('srchbtn').click();
-        await sleep(1000);
-        
-        expect(await remote.findDisplayedById('yschsp').getAttribute('value')).to.equal('test')
-        const data = await remote.takeScreenshot()
-        fs.writeFileSync('./uopolo.png', data, 'base64');
-    });
-});
+search('selenium')
